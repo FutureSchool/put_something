166c912bf9f4c0399180e01f1439207a7578569e
chandran <ch.goodchild@gmail.com>
2014-03-06 14:37:26 +0530

    Added a click handler
    Removed Kani's commented sms sending code
    
    Added "SharedPrefferences" to save the user input for next time..

--- SMS Delay Sender/src/com/example/sms/delay/sender/MainActivity.java
+++ SMS Delay Sender/src/com/example/sms/delay/sender/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sms.delay.sender;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.LoginFilter.UsernameFilterGeneric;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,39 +16,68 @@ public class MainActivity extends Activity {
 	EditText textPhoneNo;
 	EditText textSMS;
 
+	public static final String PREFS_NAME = "MyPrefsFile";
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		buttonSend = (Button) findViewById(R.id.help_button);
-		textPhoneNo = (EditText) findViewById(R.id.contact_numbers_sms);
+		//
+		// buttonSend = (Button) findViewById(R.id.help_button);
+		// textPhoneNo = (EditText) findViewById(R.id.contact_numbers_sms);
+		SharedPreferences list = getSharedPreferences(PREFS_NAME, 0);
 		
-		
-		
-		ParametersToPass par= new ParametersToPass();
+		((EditText) findViewById(R.id.users_name)).setText(list.getString("usersName", ""));
+		((EditText) findViewById(R.id.users_name)).setText(list.getString("usersAddress", ""));
+		((EditText) findViewById(R.id.users_name)).setText(list.getString("helpPhoneNumber", ""));
+		((EditText) findViewById(R.id.users_name)).setText(list.getString("helpSmsNumber", ""));
+		((EditText) findViewById(R.id.users_name)).setText(list.getString("helpEmailAddress", ""));
 
-		par.usersName = findViewById(R.id.users_name).toString();
-		par.usersAddress = findViewById(R.id.users_address).toString();
-		par.helpPhoneNumber = findViewById(R.id.contact_numbers_call).toString();
-		par.helpEmailAddress = findViewById(R.id.contact_emails).toString();
-		par.helpSmsNumber= findViewById(R.id.contact_numbers_sms).toString();
+
 		
-		
+	}
+
+	public void startSecurity(View view) {
+
+		ParametersToPass par = new ParametersToPass();
+
+		par.usersName = ((EditText) findViewById(R.id.users_name)).getText()
+				.toString();
+		par.usersAddress = ((EditText) findViewById(R.id.users_address))
+				.getText().toString();
+		par.helpPhoneNumber = ((EditText) findViewById(R.id.contact_numbers_call))
+				.getText().toString();
+		par.helpSmsNumber = ((EditText) findViewById(R.id.contact_numbers_sms))
+				.getText().toString();
+		par.helpEmailAddress = ((EditText) findViewById(R.id.contact_emails))
+				.getText().toString();
+
 		if (par.usersName.length() != 0 && par.usersAddress.length() != 0) {
-			
-			
-			if (par.helpPhoneNumber.length() != 0 && 
-					par.helpEmailAddress.length() != 0 && 
-					par.helpSmsNumber.length() != 0) {
+
+			if (par.helpPhoneNumber.length() == 0
+					&& par.helpEmailAddress.length() == 0
+					&& par.helpSmsNumber.length() == 0) {
 				Toast toast = Toast.makeText(getBaseContext(),
-						"Please fill atleast one\n" +
-						"of the contact fields,\n" +
-						"so that the app can contact\n" +
-						"people in an emergency", Toast.LENGTH_LONG);
+						"Please fill atleast one\n"
+								+ "of the contact fields,\n"
+								+ "so that the app can contact\n"
+								+ "people in an emergency", Toast.LENGTH_LONG);
 				toast.show();
 
 			} else {
+
+				SharedPreferences list = getSharedPreferences(PREFS_NAME, 0);
+				SharedPreferences.Editor editor = list.edit();
+				editor.putString("usersName", par.usersName);
+				editor.putString("usersAddress", par.usersAddress);
+				editor.putString("helpPhoneNumber", par.helpPhoneNumber);
+				editor.putString("helpEmailAddress0", par.helpEmailAddress);
+				editor.putString("helpSmsNumber", par.helpSmsNumber);
+
+				// Commit the edits!
+				editor.commit();
+
 				Intent activity_2 = new Intent(this, LockWndow.class);
 				// activity_2.putExtra(activityPath, R.id.start_path);
 
@@ -58,26 +90,5 @@ public class MainActivity extends Activity {
 					"Please enter your name and address", Toast.LENGTH_LONG);
 			toast.show();
 		}
-
-		
-//		textSMS = (EditText) findViewById(R.id.editTextSMS);
-
-//		buttonSend.setOnClickListener(new OnClickListener() {
-//
-//			public void onClick(View v) {
-//
-////				String phoneNo = textPhoneNo.getText().toString();
-////				String sms = textSMS.getText().toString();
-//				
-//				String phoneNo = textPhoneNo.getText().toString();
-//				String sms = textSMS.getText().toString();
-//
-//	
-//				
-//				Sms s = new Sms();
-//				s.SendSMS(phoneNo,sms);
-//			}
-//		});
-
 	}
 }
