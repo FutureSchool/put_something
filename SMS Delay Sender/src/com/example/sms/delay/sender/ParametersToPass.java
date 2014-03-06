bd69e4a27f486e1bfdeecb32cf423a2f43a59ca7
chandran <ch.goodchild@gmail.com>
2014-03-06 13:24:25 +0530

    2. Check if the fields are correctly and completely filled in, the Activate Alarm button should only be clickable if all fields are ready. I did it and it should work but I can't start the debugger for some reason.
    Here is the Error message:
    An internal error occurred during: "Launching New_configuration".
    Path for project must have only one segment

---
+++ SMS Delay Sender/src/com/example/sms/delay/sender/ParametersToPass.java
@@ -0,0 +1,13 @@
+package com.example.sms.delay.sender;
+
+import java.io.Serializable;
+
+public class ParametersToPass implements Serializable{
+
+	public String usersName;
+	public String usersAddress;
+	public String helpSmsNumber;
+	public String helpEmailAddress;
+	public String helpPhoneNumber;
+
+}
