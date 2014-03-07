4a09ca4b123e7b7209349a3deed068b913e0aca3
kanikannan <vkanikannan@gmail.com>
2014-03-07 12:26:10 +0530

    Lock Window with a pop up window for stopping the alarm. Have not handled the password part yet

--- SMS Delay Sender/src/com/example/sms/delay/sender/LockWndow.java
+++ SMS Delay Sender/src/com/example/sms/delay/sender/LockWndow.java
@@ -4,11 +4,16 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 public class LockWndow extends Activity {
@@ -16,7 +21,7 @@ public class LockWndow extends Activity {
 	Button help;
 	Button stop;
 	String address, name, phoneNo, sms, recipients;
-	
+
 	private MediaPlayer mp;
 
 	@Override
@@ -36,20 +41,32 @@ public class LockWndow extends Activity {
 		address = parameters.usersAddress;
 		phoneNo = parameters.helpPhoneNumber;
 		recipients = parameters.helpEmailAddress;
-	
+
 		help.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
+				inflateView();
 				dispatchSms(phoneNo, createSms(address, name));
-//				have to set permission for Internet
-//				sendEmail();
+				// have to set permission for Internet
+				// sendEmail();
 			}
 		});
 		stop.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
+				
 				stopAl();
 			}
 		});
+	}
+
+	public void inflateView() {
+		LayoutInflater inflater = (LayoutInflater) this
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.pop_up_pass,
+				(ViewGroup) findViewById(R.id.popup_element));
+		PopupWindow pw = new PopupWindow(layout, 100, 100, true);
+		pw.showAtLocation(layout, Gravity.BOTTOM, 0, 0);
+
 	}
 
 	public String createSms(String address, String name) {
@@ -79,24 +96,25 @@ public class LockWndow extends Activity {
 	public void stopAl() {
 		mp.stop();
 	}
-	
-	 protected void sendEmail() {
 
-	      Intent email = new Intent(Intent.ACTION_SEND, Uri.parse("mailto:"));
-	      // prompts email clients only
-	      email.setType("message/rfc822");
+	protected void sendEmail() {
 
-	      email.putExtra(Intent.EXTRA_EMAIL, recipients);
-	      email.putExtra(Intent.EXTRA_SUBJECT, "Help");
-	      email.putExtra(Intent.EXTRA_TEXT, "Help");
+		Intent email = new Intent(Intent.ACTION_SEND, Uri.parse("mailto:"));
+		// prompts email clients only
+		email.setType("message/rfc822");
 
-	      try {
-		    // the user can choose the email client
-	         startActivity(Intent.createChooser(email, "Choose an email client from..."));
-	     
-	      } catch (android.content.ActivityNotFoundException ex) {
-	         Toast.makeText(LockWndow.this, "No email client installed.",
-	        		 Toast.LENGTH_LONG).show();
-	      }
-	   }
+		email.putExtra(Intent.EXTRA_EMAIL, recipients);
+		email.putExtra(Intent.EXTRA_SUBJECT, "Help");
+		email.putExtra(Intent.EXTRA_TEXT, "Help");
+
+		try {
+			// the user can choose the email client
+			startActivity(Intent.createChooser(email,
+					"Choose an email client from..."));
+
+		} catch (android.content.ActivityNotFoundException ex) {
+			Toast.makeText(LockWndow.this, "No email client installed.",
+					Toast.LENGTH_LONG).show();
+		}
+	}
 }
